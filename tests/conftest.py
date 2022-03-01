0834f0d324b67c7caa64d0e8ce610eff91a9f643
rain <rain.epp@ualberta.ca>
2022-02-28 19:29:35 -0700

    Add unit tests for filterpaper

--- tests/conftest.py
+++ tests/conftest.py
@@ -1,9 +1,22 @@
 import pytest
 
 from api import app
+from api.database import db
 
 
 @pytest.fixture()
 def client():
     app.config.update({"TESTING": True})
-    return app.test_client()
+    app.config.update({"SQLALCHEMY_DATABASE_URI": "sqlite://"})
+
+    test_client = app.test_client()
+
+    with test_client.application.app_context():
+        db.engine.execute("attach ':memory:' as docs")
+        db.Model.metadata.create_all(db.engine)
+
+    yield test_client
+
+    with test_client.application.app_context():
+        db.Model.metadata.drop_all(db.engine)
+        db.engine.execute("detach docs")
