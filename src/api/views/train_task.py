68622d467058131f936f11f46c48db26d6e2a23f
Andrew Chen <akchen@ualberta.ca>
2022-04-04 15:18:46 -0600

    update num neg samples

--- src/api/views/train_task.py
+++ src/api/views/train_task.py
@@ -108,7 +108,7 @@ class SuggestHParams(MethodView):
             "embedding_size": 200,
             "epochs_to_train": 15,
             "learning_rate": 0.025,
-            "num_neg_samples": 25,
+            "num_neg_samples": 5,
             "batch_size": 500,
             "concurrent_steps": 12,
             "window_size": 5,
